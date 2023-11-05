4903610bc3036549be9c53d3298b6909aedeafde
AgustinChavero <agustindanielchavero@gmail.com>
2023-11-05 00:59:40 -0300

    Bases for controllers & global service

--- src/modules/film/film-controller.js
+++ src/modules/film/film-controller.js
@@ -1,6 +1,8 @@
+const Film = require("./film-model");
 const { ClientError } = require("../../utils/errors/error");
 const { customResponse } = require("../../utils/errors/custom-response");
 const { create } = require("./film-service");
+const { globalService } = require("../../utils/global-service/global-service");
 
 const postFilm = (req, res) => {
   const { body: data } = req;
@@ -11,11 +13,42 @@ const postFilm = (req, res) => {
 
   customResponse(res, 200, { message: "Film created", newFilm });
 };
-const putFilm = (req, res) => {};
-const patchFilm = (req, res) => {};
+
+const putFilm = (req, res) => {
+  const { body } = req;
+  const { id } = req.params;
+
+  const film = globalService.updateElement(id, body, Film, "Film");
+
+  customResponse(res, 200, { message: "Film updated", film });
+};
+
+const patchFilm = (req, res) => {
+  const { body } = req;
+  const { id } = req.params;
+
+  const film = globalService.patchElement(id, body, Film, "Film");
+
+  customResponse(res, 200, { message: "Film updated", film });
+};
+
 const getAllFilm = (req, res) => {};
-const getFilm = (req, res) => {};
-const deleteFilm = (req, res) => {};
+
+const getFilm = (req, res) => {
+  const { id } = req.params;
+
+  const film = globalService.findElement(id, Film, "Film");
+
+  customResponse(res, 200, { message: "Film finded", film });
+};
+
+const deleteFilm = (req, res) => {
+  const { id } = req.params;
+
+  const film = globalService.deleteElement(id, Film, "Film");
+
+  customResponse(res, 200, { message: "Film deleted", film });
+};
 
 module.exports = {
   postFilm,
--- src/utils/global-service/global-service.js
+++ src/utils/global-service/global-service.js
@@ -1,20 +1,13 @@
 const { ClientError } = require("../utils/error/error");
 
-const deleteElement = async (id, model, name) => {
-  const elementToDelete = await model.findById(id);
-  if (!elementToDelete) throw new ClientError(`${name} not found`);
+const updateElement = async (id, body, model, name) => {
+  const elementToUpdate = await model.findById(id);
+  if (!elementToUpdate) throw new ClientError(`${name} not found`);
 
-  elementToDelete.isDeleted = true;
-  await elementToDelete.save();
+  elementToUpdate.set(body);
+  await elementToUpdate.save();
 
-  return elementToDelete;
-};
-
-const findElement = async (id, model, name) => {
-  const elementToFind = await model.findById(id);
-  if (!elementToFind) throw new ClientError(`${name} not found`);
-
-  return elementToFind;
+  return elementToUpdate;
 };
 
 const patchElement = async (id, body, model, name) => {
@@ -29,21 +22,28 @@ const patchElement = async (id, body, model, name) => {
   return elementToPatch;
 };
 
-const updateElement = async (id, body, model, name) => {
-  const elementToUpdate = await model.findById(id);
-  if (!elementToUpdate) throw new ClientError(`${name} not found`);
+const findElement = async (id, model, name) => {
+  const elementToFind = await model.findById(id);
+  if (!elementToFind) throw new ClientError(`${name} not found`);
 
-  elementToUpdate.set(body);
-  await elementToUpdate.save();
+  return elementToFind;
+};
 
-  return elementToUpdate;
+const deleteElement = async (id, model, name) => {
+  const elementToDelete = await model.findById(id);
+  if (!elementToDelete) throw new ClientError(`${name} not found`);
+
+  elementToDelete.isDeleted = true;
+  await elementToDelete.save();
+
+  return elementToDelete;
 };
 
 module.exports = {
   globalService: {
-    deleteElement,
-    findElement,
-    patchElement,
     updateElement,
+    patchElement,
+    findElement,
+    deleteElement,
   },
 };
